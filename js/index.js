4864e2210c5fc366ba10c35d2a0c4717ba4973e6
Dan Dart <git@dandart.co.uk>
2022-12-11 06:22:17 +0000

    remove space before eol

--- js/index.js
+++ js/index.js
@@ -14,40 +14,40 @@ class Game {
         this.speed = 5;
         this.rotspeed = 5;
     }
-    
+
     move(dx, dy) {
         this.playerx += dx;
         this.playery += dy;
         this.sync();
     }
-    
+
     rotate(dx, dy) {
         this.rot = Math.atan2(dy, dx) || this.rot;
         this.sync();
     }
-    
+
     sync() {
         this.elPlayer.style.top = (this.height - this.playery) + 'px';
         this.elPlayer.style.left = this.playerx + 'px';
         this.elPlayer.style.transform = 'rotate(' + (90 + todeg(this.rot)) + 'deg)';
     }
-    
+
     gpConnected(ev) {
         this.gamepads.push(ev.gamepad);
         this.animate();
     }
-    
+
     frame() {
         this.move(this.gamepads[0].axes[0] * this.speed, -this.gamepads[0].axes[1] * this.speed);
         this.rotate(this.gamepads[0].axes[2] * this.rotspeed, this.gamepads[0].axes[3] * this.rotspeed);
         //console.log(this.gamepads[0].axes);
     }
-    
+
     animate() {
         this.frame();
         requestAnimationFrame(this.animate.bind(this));
     }
-    
+
     events() {
         window.addEventListener('gamepadconnected', this.gpConnected.bind(this));
     }
